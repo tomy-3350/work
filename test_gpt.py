b829b060f601b996c56caff61237c76693a6b080
とむ <ttiih@outlook.jp>
2025-07-17 20:38:18 +0900

    ver_gpt

---
+++ test_gpt.py
@@ -0,0 +1,83 @@
+import gspread
+import streamlit as st
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Streamlit SecretsからTOML形式で情報を取得
+google_cloud_secret = st.secrets["google_cloud"]
+
+# Secretsから必要な情報を構築
+service_account_info = {
+    "type": google_cloud_secret["type"],
+    "project_id": google_cloud_secret["project_id"],
+    "private_key_id": google_cloud_secret["private_key_id"],
+    "private_key": google_cloud_secret["private_key"],  # 改行が含まれる
+    "client_email": google_cloud_secret["client_email"],
+    "client_id": google_cloud_secret["client_id"],
+    "auth_uri": google_cloud_secret["auth_uri"],
+    "token_uri": google_cloud_secret["token_uri"],
+    "auth_provider_x509_cert_url": google_cloud_secret["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": google_cloud_secret["client_x509_cert_url"],
+    "universe_domain": google_cloud_secret["universe_domain"]
+}
+
+# サービスアカウント認証情報を`ServiceAccountCredentials`に渡す
+creds = ServiceAccountCredentials.from_json_keyfile_dict(service_account_info,
+                                                         ["https://spreadsheets.google.com/feeds",
+                                                          "https://www.googleapis.com/auth/spreadsheets",
+                                                          "https://www.googleapis.com/auth/drive.file",
+                                                          "https://www.googleapis.com/auth/drive"])
+
+# gspreadを使ってGoogle Sheets APIに認証
+gc = gspread.authorize(creds)
+
+# タイトル
+st.title('北青 機械課 作業日報')
+st.caption("メーカー名、工番、作業内容、時間を入力してください。")
+day = st.date_input("日付を選択してください")
+
+# 名前
+name = st.selectbox('名前', ('選択してください', '大地', '山岸', '坂本', '一條', '松本', '将', '出繩'))
+
+
+# メーカー選択用の関数
+def create_task_section(task_num):
+    customer = st.selectbox(f'メーカー{task_num}',
+                            ['選択してください', 'ジーテクト', 'ヨロズ', '城山', 'タチバナ', '浜岳', '三池', '東プレ',
+                             '千代田', '武部', 'インフェック', 'その他'])
+    new_customer = st.text_input(f'メーカー名を入力{task_num}', disabled=(customer != 'その他'))
+    genre = st.selectbox(f'作業内容{task_num}',
+                         ['選択してください', '新規', '改修', 'その他']) if customer != '選択してください' else None
+    number = st.text_input(f'工番を入力{task_num}', disabled=(genre is None))
+    time = st.number_input(f'時間を入力{task_num}', min_value=0) if number else None
+    return customer, new_customer, genre, number, time
+
+
+# 各メーカーセクションの生成
+tasks = []
+for i in range(1, 6):
+    task = create_task_section(i)
+    tasks.append(task)
+
+# 合計時間の計算
+total_time = sum([task[4] for task in tasks if task[4] is not None])
+if total_time > 0:
+    st.text(f'合計: {total_time}時間')
+
+# Google Sheetsにデータを送信
+submit_btn = st.button('送信')
+if submit_btn:
+    st.text('お疲れ様でした！')
+
+    # データ送信のために行を作成
+    row_data = [str(day) + name]  # 日付と名前を最初に追加
+
+    for task in tasks:
+        customer, new_customer, genre, number, time = task
+        if customer != '選択してください':
+            row_data.extend([new_customer if new_customer else customer, genre, number, time])
+        else:
+            row_data.extend(['', '', '', ''])
+
+    # シートにデータを追加
+    sheet = gc.open("python").sheet1
+    sheet.append_row(row_data)
