2b37c61d287416effbda8386e813bf3e1dba2375
とむ <ttiih@outlook.jp>
2025-07-17 16:52:10 +0900

    ver_test

--- test.py
+++ test.py
@@ -2,12 +2,35 @@ import gspread
 import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope =  ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+# Streamlit SecretsからTOML形式で情報を取得
+google_cloud_secret = st.secrets["google_cloud"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("sheet-test.json",scope)
+# Secretsから必要な情報を構築
+service_account_info = {
+    "type": google_cloud_secret["type"],
+    "project_id": google_cloud_secret["project_id"],
+    "private_key_id": google_cloud_secret["private_key_id"],
+    "private_key": google_cloud_secret["private_key"],
+    "client_email": google_cloud_secret["client_email"],
+    "client_id": google_cloud_secret["client_id"],
+    "auth_uri": google_cloud_secret["auth_uri"],
+    "token_uri": google_cloud_secret["token_uri"],
+    "auth_provider_x509_cert_url": google_cloud_secret["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": google_cloud_secret["client_x509_cert_url"],
+    "universe_domain": google_cloud_secret["universe_domain"]
+}
 
+# サービスアカウント認証情報を`ServiceAccountCredentials`に渡す
+creds = ServiceAccountCredentials.from_json_keyfile_dict(service_account_info,
+    ["https://spreadsheets.google.com/feeds",
+     "https://www.googleapis.com/auth/spreadsheets",
+     "https://www.googleapis.com/auth/drive.file",
+     "https://www.googleapis.com/auth/drive"])
+
+# gspreadを使ってGoogle Sheets APIに認証
 gc = gspread.authorize(creds)
 
+# シートを開く
 sheet = gc.open("python").sheet1
 
 
