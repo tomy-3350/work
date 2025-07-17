bb517cc8b786e53ed8b4a39f8a09bdfeed3c2c35
とむ <ttiih@outlook.jp>
2025-07-17 20:23:02 +0900

    ver_true

--- test.py
+++ test.py
@@ -107,22 +107,166 @@ if number != '':
 else:
     pass
 
+###################################
+# メーカー2
+if time != 0:
+    customer2 = st.selectbox(
+    'メーカー2',
+    ('選択してください', 'ジーテクト', 'ヨロズ', '城山', 'タチバナ', '浜岳', '三池', '東プレ',
+     '千代田', '武部', 'インフェック', 'その他'))
+else:
+    pass
 
+# 新規メーカー2
+if customer2 == 'その他':
+    new_customer2 = st.text_input('メーカー名を入力')
+else:
+    pass
+
+# 作業区分2
+if customer2 != '選択してください':
+    genre2 = st.selectbox(
+    '作業内容2',
+    ('選択してください','新規','改修','その他'))
+else:
+    pass
+
+# 工番入力2
+if genre2 != '選択してください':
+    number2 = st.text_input('工番を入力2')
+else:
+    pass
+
+# 時間2
+if number2 != '':
+    time2 = st.number_input('時間を入力2')
+else:
+    pass
+
+######################################
+# メーカー3
+if time2 != 0:
+    customer3 = st.selectbox(
+    'メーカー3',
+    ('選択してください', 'ジーテクト', 'ヨロズ', '城山', 'タチバナ', '浜岳', '三池', '東プレ',
+    '千代田', '武部', 'インフェック'))
+else:
+    pass
+
+# 作業区分3
+if customer3 != '選択してください':
+    genre3 = st.selectbox(
+    '作業内容3',
+    ('選択してください','新規','改修','その他'))
+else:
+    pass
+
+# 工番入力3
+if genre3 != '選択してください':
+    number3 = st.text_input('工番を入力3')
+else:
+    pass
+
+# 時間3
+if number3 != '':
+    time3 = st.number_input('時間を入力3')
+else:
+    pass
+
+###############################
+# メーカー4
+if time3 != 0:
+    customer4 = st.selectbox(
+    'メーカー4',
+    ('選択してください', 'ジーテクト', 'ヨロズ', '城山', 'タチバナ', '浜岳', '三池', '東プレ',
+    '千代田', '武部', 'インフェック'))
+else:
+    pass
+
+# 作業区分4
+if customer4 != '選択してください':
+    genre4 = st.selectbox(
+    '作業内容4',
+    ('選択してください','新規','改修','その他'))
+else:
+    pass
+
+# 工番入力4
+if genre4 != '選択してください':
+    number4 = st.text_input('工番を入力4')
+else:
+    pass
+
+# 時間4
+if number4 != '':
+    time4 = st.number_input('時間を入力4')
+else:
+    pass
+
+###########################
+# メーカー5
+if time4 != 0:
+    customer5 = st.selectbox(
+    'メーカー5',
+    ('選択してください', 'ジーテクト', 'ヨロズ', '城山', 'タチバナ', '浜岳', '三池', '東プレ',
+    '千代田', '武部', 'インフェック'))
+else:
+    pass
+
+# 作業区分5
+if customer5 != '選択してください':
+    genre5 = st.selectbox(
+    '作業内容5',
+    ('選択してください','新規','改修','その他'))
+else:
+    pass
+
+# 工番入力5
+if genre5 != '選択してください':
+    number5 = st.text_input('工番を入力5')
+else:
+    pass
+
+# 時間5
+if number5 != '':
+    time5 = st.number_input('時間を入力5')
+else:
+    pass
+
+##################
 total1 = time
 if time != 0:
     st.text('合計' + str(total1) + '時間')
 
-# シートを開く
+########### シートを開く ##############
 sheet = gc.open("python").sheet1
 
-
-#==================
-# ボタン
+############ ボタン #################
 submit_btn = st.button('送信')
 if submit_btn:
     st.text('お疲れ様でした！')
 
-if submit_btn and new_customer == '選択してください':
+if submit_btn and new_customer == '選択してください' and customer2 == '選択してください':
     sheet.append_row([str(day)+name,customer,genre,number,time])
-elif submit_btn and new_customer != '選択してください':
+elif submit_btn and new_customer != '選択してください' and customer2 == '選択してください':
     sheet.append_row([str(day)+name, new_customer, genre, number, time])
+
+elif submit_btn and new_customer == '選択してください' and customer2 != '選択してください':
+    sheet.append_row([str(day)+name,customer,genre,number,time,customer2,genre2,number2,time2])
+elif submit_btn and new_customer != '選択してください' and new_customer2 != '選択してください':
+    sheet.append_row([str(day)+name,new_customer,genre,number,time,new_customer2,genre2,number2,time2])
+
+elif submit_btn and new_customer == '選択してください' and customer3 != '選択してください':
+    sheet.append_row([str(day)+name,customer,genre,number,time,customer2,genre2,number2,time2,customer3,genre3,number3,time3])
+elif submit_btn and new_customer != '選択してください' and new_customer2 != '選択してください' and customer3 != '選択してください':
+    sheet.append_row([str(day)+name,new_customer,genre,number,time,new_customer2,genre2,number2,time2,customer3,genre3,number3,time3])
+
+elif submit_btn and new_customer == '選択してください' and customer4 != '選択してください':
+    sheet.append_row([str(day)+name,customer,genre,number,time,customer2,genre2,number2,time2,customer3,genre3,number3,time3,customer4,genre4,number4,time4])
+elif submit_btn and new_customer != '選択してください' and new_customer2 != '選択してください' and customer4 != '選択してください':
+    sheet.append_row([str(day)+name,new_customer,genre,number,time,new_customer2,genre2,number2,time2,customer3,genre3,number3,time3,customer4,genre4,number4,time4])
+
+elif submit_btn and new_customer == '選択してください' and customer5 != '選択してください':
+    sheet.append_row([str(day)+name,customer,genre,number,time,customer2,genre2,number2,time2,customer3,genre3,number3,time3,customer4,genre4,number4,time4,customer5,genre5,number5,time5])
+elif submit_btn and new_customer != '選択してください' and new_customer2 != '選択してください' and customer5 != '選択してください':
+    sheet.append_row([str(day)+name,new_customer,genre,number,time,new_customer2,genre2,number2,time2,customer3,genre3,number3,time3,customer4,genre4,number4,time4,customer5,genre5,number5,time5])
